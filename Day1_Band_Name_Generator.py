1b3af1d14036344ccffce1ea3d39ca6ba14a17f5
Paduchuri Manideep <paduchurimanideep@gmail.com>
2021-04-03 17:05:10 +0530

    Band Name Generation Project

---
+++ Day1_Band_Name_Generator.py
@@ -0,0 +1,14 @@
+#Author: Manideep Paduchuri
+#Objective - Suggest band name based on city and pet name of the end user
+
+#Greeting
+print("Welcome to the Band Name Generator.")
+
+#Ask city input from User
+city = input("What's name of the city you grew up in?\n")
+
+#Ask pet name input from User
+pet_name = input("What's your pet's name?\n")
+
+#Suggest Band Name based on pet name and city name of User
+print("Your band name could be "+ city + " " + pet_name)
