a5e98dd97c7635d018079fb89fa68e8b5b093e9d
Paduchuri Manideep <paduchurimanideep@gmail.com>
2021-04-12 17:08:07 +0530

    Added Greeting & Formatting for Tip Calculator

--- Day2/Bill_Tip_Calculator.py
+++ Day2/Bill_Tip_Calculator.py
@@ -7,13 +7,10 @@
 #HINT 1: https://www.google.com/search?q=how+to+round+number+to+2+decimal+places+python&oq=how+to+round+number+to+2+decimal
 #HINT 2: https://www.kite.com/python/answers/how-to-limit-a-float-to-two-decimal-places-in-python
 
+print("Welcome to the Tip Calculator!")
 total_bill = float(input("What was the total bill? "))
 tip_per = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
 persons = int(input("How many persons to split the bill? "))
-
-#Calculate the total Bill
 total_bill = total_bill +  total_bill*(tip_per/100)
-
-#Bill for each person
-bill_to_pay = round(total_bill/persons,2)
+bill_to_pay = "{:.2f}".format(round(total_bill/persons,2))
 print(f"Each persons should pay: {bill_to_pay}")
