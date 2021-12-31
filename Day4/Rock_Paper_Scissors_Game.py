c82ae977be9c1bb56203307838beba20a9907ed0
Paduchuri Manideep <paduchurimanideep@gmail.com>
2021-12-31 18:49:27 +0530

    Update Rock_Paper_Scissors_Game.py

--- Day4/Rock_Paper_Scissors_Game.py
+++ Day4/Rock_Paper_Scissors_Game.py
@@ -5,6 +5,8 @@ Rock wins over scissors (because rock smashes scissors)
 Scissors wins over paper (because scissors cut paper)
 Paper wins over rock (because paper covers rock)
 '''
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -43,27 +45,28 @@ sym_dict = {
 #Get the User's choice
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-#Get the computers Choice using random module
-import random
-comp_choice = random.randint(0,2)
-
-#Print User & Computer Choice
-print("User Choice is:")
-print(sym_dict[user_choice])
-print("\n\n")
-print("Computer Choice is:")
-print(sym_dict[comp_choice])
-
-#Comparision & Determing the result
+#Validated the User Choice
 if user_choice > 2 or user_choice < 0:
   print("You choose an Invalid choice. You Lose!!")
-elif user_choice == comp_choice:
-  print("You Draw!!")
-elif user_choice == 0 and comp_choice == 2:
-  print("You Win!!")
-elif user_choice > comp_choice:
-  print("You Win!!")
-elif user_choice < comp_choice:
-  print("You Lose!!")
-else:
-  print("You Lose!!")
+else: #If user choice is valid, proceed further
+  #Get the computers Choice using random module
+  comp_choice = random.randint(0,2)
+  #Print User & Computer Choice
+  print("User Choice is:")
+  print(sym_dict[user_choice])
+  print("\n\n")
+  print("Computer Choice is:")
+  print(sym_dict[comp_choice])
+  #Validate choices & Determine result
+  if user_choice == comp_choice:
+    print("You Draw!!")
+  elif user_choice == 0 and comp_choice == 2:
+    print("You Win!!")
+  elif user_choice == 2 and comp_choice == 0:
+    print("You Lose!!")
+  elif user_choice > comp_choice:
+    print("You Win!!")
+  elif user_choice < comp_choice:
+    print("You Lose!!")
+  else:
+    print("You Lose!!")
